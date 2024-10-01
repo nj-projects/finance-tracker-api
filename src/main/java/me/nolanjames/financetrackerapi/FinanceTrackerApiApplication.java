0d6eba08d86b7faefa6ede3cb0c82a39746cc139
nj-projects <jn@jamesnolan.me>
2024-10-01 09:23:17 +0100

    initial commit

---
+++ src/main/java/me/nolanjames/financetrackerapi/FinanceTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package me.nolanjames.financetrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceTrackerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/nolanjames/financetrackerapi/FinanceTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nolanjames.financetrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
